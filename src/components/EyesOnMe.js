741262418307cf59ba75536184cbf5090012365b
Taylor Williams <taylor@thewilliams.net>
2020-03-19 20:44:07 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,5 +1,12 @@
 // Code EyesOnMe Component Here
 class EyesOnMe extends React.Component{
+
+  focus = () => {
+    console.log("Good!")
+  }
+  blur = () => {
+    console.log("Hey! Eyes on me!")
+  }
   render() {
     return <button onFocus={this.focus} onBlur={this.blur} />
   }
